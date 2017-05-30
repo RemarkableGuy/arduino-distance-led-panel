6e2d891c982471e7630c16b74b71bcb6d18473b5
Alvaro Schipper <alvaro.schipper@hotmail.com>
2017-05-30 22:04:49 +0200

    Added moving average

--- distance_led.ino
+++ distance_led.ino
@@ -24,21 +24,59 @@ int URTRIG = 12;       // PWM trigger pin
 unsigned int distance = 0;
 
 int color = 0;
- 
+
+// Define the number of samples to keep track of.  The higher the number,
+// the more the readings will be smoothed, but the slower the output will
+// respond to the input.  Using a constant rather than a normal variable lets
+// use this value to determine the size of the readings array.
+const int numReadings = 10;
+
+int readings[numReadings];      // the readings from the analog input
+int readIndex = 0;              // the index of the current reading
+int total = 0;                  // the running total
+int average = 0;                // the average
+
 uint8_t EnPwmCmd[4]={0x44,0x22,0xbb,0x01}; // distance measure command
 
 RGBmatrixPanel matrix(A, B, C, D, CLK, LAT, OE, false);
   
 void setup() {             // Serial initialization
     Serial.begin(9600);    // Sets the baud rate to 9600
+    // initialize all the readings to 0:
+    for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+      readings[thisReading] = 0;
+    }
     PWM_Mode_Setup();
     matrix.begin();
 }
   
 void loop() {
   distance = PWM_Mode();
-  Serial.println(distance);
-  matrix.fillScreen(matrix.ColorHSV(distance * 10, 255, 255, true));
+  // subtract the last reading:
+  total = total - readings[readIndex];
+  // read from the sensor:
+  readings[readIndex] = distance;
+  // add the reading to the total:
+  total = total + readings[readIndex];
+  // advance to the next position in the array:
+  readIndex = readIndex + 1;
+
+  // if we're at the end of the array...
+  if (readIndex >= numReadings) {
+    // ...wrap around to the beginning:
+    readIndex = 0;
+  }
+
+  // calculate the average:
+  average = total / numReadings;
+  
+  //Serial.println(distance);
+  //Serial.println(average);
+  // send it to the computer as ASCII digits
+    matrix.fillCircle(0,0,22.5,matrix.ColorHSV(average * 10, 255, 255, true));
+    matrix.fillCircle(matrix.width(),matrix.height(),22.5,matrix.ColorHSV(distance * 10, 255, 255, true));
+  //matrix.drawRect(0,0,16,16,matrix.ColorHSV(average * 10, 255, 255, true));
+  //matrix.fillScreen(matrix.ColorHSV(average * 10, 255, 255, true));
   delay(20);
 }
  
